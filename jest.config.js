6bc989efb0d51bad27e1003f559005e6aa6c2191
maff1989 <matthew@urgero.net>
2025-12-22 20:47:00 -0700

    Add missing npm scripts, dependencies
    This commit adds the requisite packages for proper formatting, linting, and testing. The appropriate npm package scripts are also added to `package.json`.
    
    Also, the existing formatting/linting configuration is updated, and the Jest testing configuration is added.

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/test/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'lib/**/*.ts',
+    'utils/**/*.ts',
+    '!lib/**/*.d.ts',
+    '!utils/**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/dist/**',
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/test/',
+    '/examples/',
+  ],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+      tsconfig: {
+        module: 'ES2020',
+        target: 'ES2020',
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true,
+      },
+    }],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+}
